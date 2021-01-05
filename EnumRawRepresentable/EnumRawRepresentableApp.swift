829b47e473d3b1f9084fd8763863691bc9372193
Bill Morrison <bill1.morrison1@gmail.com>
2021-01-04 16:07:57 -0800

    Initial Commit

---
+++ EnumRawRepresentable/EnumRawRepresentableApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnumRawRepresentableApp.swift
+//  EnumRawRepresentable
+//
+//  Created by Bill Morrison on 1/4/21.
+//
+
+import SwiftUI
+
+@main
+struct EnumRawRepresentableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
